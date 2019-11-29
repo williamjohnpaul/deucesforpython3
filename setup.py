9e494e4bd6673f830e280ebdc9e706134bb94413
bill <williamjohnpaulrowe@gmail.com>
2019-11-29 14:14:08 -0500

    Add project files.

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+Deuces: A pure Python poker hand evaluation library
+"""
+
+from setuptools import setup
+
+setup(
+    name='deucesp3',
+    version='0.1',
+    description=__doc__,
+    #long_description=open('README.md').read(),\
+    long_description="long_description",
+    author='Will Drevo',
+    url='https://github.com/worldveil/deucesp3',
+    license='MIT',
+    packages=['deucesp3'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Games/Entertainment'
+    ]
+)
